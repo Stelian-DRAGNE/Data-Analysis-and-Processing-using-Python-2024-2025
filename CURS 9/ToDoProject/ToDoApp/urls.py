2da137b225881eccb960a1a5886025792cfbe06c
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-06-02 14:55:35 +0300

    Delete CURS 9 directory

--- CURS 9/ToDoProject/ToDoApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import todo_details_view
-from .views import todo_list_view
-
-urlpatterns = [
-
-	path("list", todo_list_view),
-	path("details", todo_details_view),
-]
